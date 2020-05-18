2201798c2b7e02501bd525f00281ad30b5e083b6
usrfriendly <usrfriendly@github.com>
2020-05-17 22:43:18 -0700

    new project started

---
+++ ui/diceroll.py
@@ -0,0 +1,28 @@
+import random;
+
+class Die:
+    """This generates the roll"""
+    def __init__(faceCount):
+        faceCount = faceCount
+    def Roll(faceCount):
+        return(random.randint(1,faceCount))
+
+
+
+class Roll:
+    def __init__(self,diceCount, faceCount):
+        diceCount = diceCount
+        faceCount = faceCount
+    def DoRolls(diceCount, faceCount):
+        i = 0
+        rolls = diceCount
+        #roll = random.randint(1,faceCount)
+        value = 0
+        while i < rolls:
+            roll = random.randint(1,faceCount)
+            value = value + roll
+            print(roll)
+            i += 1
+       
+        return value
+
---
+++ ui/mainwindow.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing MainWindow.
+"""
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow
+
+from .Ui_mainwindow import Ui_MainWindow
+
+from ui.diceroll import Die, Roll
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+        
+        @param parent reference to the parent widget
+        @type QWidget
+        """
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+    
+    @pyqtSlot()
+    def on_rollButton_released(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        
+        self.d4Roll.setText(str(Roll.DoRolls(self.d4SpinBox.value(), 4)))
+        self.d6Roll.setText(str(Roll.DoRolls(self.d6SpinBox.value(), 6)))
+        self.d8Roll.setText(str(Roll.DoRolls(self.d8SpinBox.value(), 8)))
+        self.d10Roll.setText(str(Roll.DoRolls(self.d10SpinBox.value(), 10)))
+        self.d12Roll.setText(str(Roll.DoRolls(self.d12SpinBox.value(), 12)))
+        self.d20Roll.setText(str(Roll.DoRolls(self.d20SpinBox.value(), 20)))
